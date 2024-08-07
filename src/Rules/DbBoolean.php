b5564f95a172632fc082d988980216ad97818ef8
Julien Roche <julien.roche@soyhuce.fr>
2024-08-07 15:26:03 +0200

    fix(DbBoolean): use ValidationDataAware in order to retrieve attributes and messages for parent validator

--- src/Rules/DbBoolean.php
+++ src/Rules/DbBoolean.php
@@ -2,16 +2,46 @@
 
 namespace Soyhuce\Rules\Rules;
 
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\ValidatorAwareRule;
+use Illuminate\Support\Facades\Validator as ValidatorFacade;
+use Illuminate\Validation\Validator;
 use Soyhuce\Rules\Concerns\DatabaseRelatedRule;
+use function in_array;
+use function is_array;
 
-class DbBoolean extends CompoundRule
+class DbBoolean implements ValidationRule, ValidatorAwareRule
 {
     use DatabaseRelatedRule;
 
-    public function __construct()
+    private Validator $validator;
+
+    /**
+     * @param Closure(string): \Illuminate\Translation\PotentiallyTranslatedString $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        parent::__construct([
-            'in:' . implode(',', $this->booleans()->all()),
-        ]);
+        if (!is_array($value) && in_array($value, $this->booleans()->all(), true)) {
+            return;
+        }
+
+        $validator = ValidatorFacade::make(
+            [],
+            [],
+            $this->validator->customMessages,
+            $this->validator->customAttributes,
+        );
+
+        $validator->addFailure($attribute, 'boolean');
+
+        $fail($validator->errors()->first($attribute));
+    }
+
+    public function setValidator(Validator $validator): self
+    {
+        $this->validator = $validator;
+
+        return $this;
     }
 }
